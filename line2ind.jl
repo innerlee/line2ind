d6b53e634766315737165309594a91f03206d2e0
innerlee <363664470@qq.com>
2015-10-12 12:29:39 +0800

    fast finally

--- line2ind.jl
+++ line2ind.jl
@@ -16,10 +16,10 @@ for i = 1:10
 end
 
 function line2ind_fast(i,n)
-  tot = n*(n-1)/2
-  x = ceil((sqrt(9+8*(tot-i))-1)/2)
-  y = n - (tot - i - (x-1)*x/2)
-  (convert(Int, n-x), convert(Int, y))
+  t = n*(n-1)/2 - i
+  x = ceil((sqrt(9+8*t)-1)/2)
+  y = n - t + (x-1)*x/2
+  (x,y)
 end
 
 println("fast")
